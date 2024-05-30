1f56f0674a038b0ad3b45c12b3d7a84333fc2f6f
kartofun <pavelyg@mail.ru>
2024-05-30 15:16:05 +0300

    remove useless imports

--- src/food_recommend/main.py
+++ src/food_recommend/main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 # from services.parser import ParserSelenium
 # from pathlib import Path
