29c4b03e91be6b056da3015d73eda9a3ff17691b
kartofun <pavelyg@mail.ru>
2024-06-03 18:58:05 +0300

    try fix fetch response

--- src/food_recommend/main.py
+++ src/food_recommend/main.py
@@ -15,12 +15,13 @@ async def root():
     # return FileResponse("static/index.html")
     return RedirectResponse(url="/static/index.html")
 
+
 @app.get("/recipes")
 async def recipe():
     recipe_links = ["1_link"]
     # recipe_links = ParserSelenium.parse_recipe_links("https://andychef.ru/recipe")
     print(recipe_links)
-    return {"message": str(recipe_links[0])}
+    return {"data": "yes"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
--- src/food_recommend/static/like_button.js
+++ src/food_recommend/static/like_button.js
@@ -2,44 +2,52 @@
 // const recipe = "test_food"
 const e = React.createElement;
 
-// async function getRecipe(){
-//   const response = await fetch("/recipes", {
-//     method: "GET",
-//     headers: {"Accept": "application/json"}
-//   });
-//   const recipe = await response.json();
-//   window.alert(recipe.json())
-//   if (response.ok == true) {
-//     const recipe = await response.json();
-//     console.log(recipe)
-//     // document.getElementById("likebutton").value = recipe
-//     return recipe
-//     // return recipe
-//   }
-// }
+async function getRecipe(){
+  const response = await fetch("/recipes", {
+    method: "GET",
+    headers: {"Accept": "application/json"}
+  });
+  const recipe = await response.json();
+  window.alert(recipe.json())
+  if (response.ok == true) {
+    const recipe = await response.json();
+    console.log(recipe)
+    // document.getElementById("likebutton").value = recipe
+    return recipe
+    // return recipe
+  }
+}
 
 class LikeButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = { liked: false };
     this.state = {date: new Date()};
+    this.state = {recipe: "null recipe"};
   }
   componentDidMount() {
     this.timerID = setInterval(
-      () => this.tick(),
+      () => this.load_data(),
       1000
     );
   }
-  tick() {
-    this.setState({
-      date: new Date()
-    });
+  load_data = () => {
+    fetch('/recipes', {
+    headers: {'Content-Type':'application/json'},
+    mode: 'cors',
+    method: httpMethod,
+    body: JSON.stringify(params)
+  })
+    .then(response => response.json())
+    .then(data => this.setState({
+      recipe: data
+    }));
   }
 
   render() {
     if (this.state.liked) {
       // const element = new Date().toLocaleTimeString();
-      const element = this.state.date.toLocaleTimeString();
+      const element = this.state.recipe;
       // const response = await fetch("/recipes", {
       //   method: "GET",
       //   headers: {"Accept": "application/json"}
