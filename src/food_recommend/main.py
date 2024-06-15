ca896785c3969055e15928cc78cb4e4e2dbd9589
kartofun <pavelyg@mail.ru>
2024-06-15 23:03:13 +0300

    remove deprecated code

--- src/food_recommend/main.py
+++ src/food_recommend/main.py
@@ -11,35 +11,25 @@ app = FastAPI(
 )
 
 app.include_router(router_pages)
-recipe_links = ParserSelenium.parse_recipe_links("https://andychef.ru/recipe")
+# recipe_links = ParserSelenium.parse_recipe_links("https://andychef.ru/recipe")
 
-origins = [
-    "http://127.0.0.1:8000",
-]
+# origins = [
+#     "http://127.0.0.1:8000",
+# ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"],
-    allow_headers=["Content-Type", "Set-Coockie", "Access-Control-Allow-Headers"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"],
+#     allow_headers=["Content-Type", "Set-Coockie", "Access-Control-Allow-Headers"],
+# )
+recipe_links = [1,2,3,4,5,6]
+
 
 @app.get("/recipes")
 def get_recipes(limit: int = 5):
     return recipe_links[:limit]
-
-# app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# @app.get("/")
-# async def root():
-#     return RedirectResponse(url="/static/index.html")
-
-# @app.get("/recipes")
-# async def recipe():
-#     recipe_links = ParserSelenium.parse_recipe_links("https://andychef.ru/recipe")
-#     print(recipe_links)
-#     return {"data": recipe_links[0]}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
