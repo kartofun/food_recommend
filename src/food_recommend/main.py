7cec183f062f4d56f4da8303124f9387c3e13c3e
kartofun <pavelyg@mail.ru>
2024-06-16 14:11:13 +0300

    validating with pydantic

--- src/food_recommend/main.py
+++ src/food_recommend/main.py
@@ -1,3 +1,5 @@
+from typing import List
+from pydantic import BaseModel
 import uvicorn
 from fastapi import FastAPI
 # from fastapi.responses import RedirectResponse
@@ -13,23 +15,20 @@ app = FastAPI(
 app.include_router(router_pages)
 # recipe_links = ParserSelenium.parse_recipe_links("https://andychef.ru/recipe")
 
-# origins = [
-#     "http://127.0.0.1:8000",
-# ]
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"],
-#     allow_headers=["Content-Type", "Set-Coockie", "Access-Control-Allow-Headers"],
-# )
 recipe_links = [1,2,3,4,5,6]
+
+class Dessert(BaseModel):
+    calories: int
+    price: float
 
 
 @app.get("/recipes")
 def get_recipes(limit: int = 5):
     return recipe_links[:limit]
+
+@app.post("/dessert")
+def get_desserts(desserts: List[Dessert]):
+    pass
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
